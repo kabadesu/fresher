fc6313720beb39edb13be89f89bb848616eb848a
Murray Nuttall <murray.nuttall@gmail.com>
2018-03-11 10:46:02 +1300

    Added eslint airbnb base for atom. Still need to add gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,47 @@
-var gulp = require('gulp');
-var gulpLoadPlugins = require('gulp-load-plugins');
-var $ = gulpLoadPlugins();
-var autoprefixer = require('autoprefixer');
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const autoprefixer = require('autoprefixer');
 
-var plugins = [
-    autoprefixer({browsers: [
-        'last 2 versions',
-        '> 5%'
-    ]})
-]
+const $ = gulpLoadPlugins();
 
-gulp.task('webserver', function() {
+const plugins = [
+    autoprefixer({
+        browsers: [
+            'last 2 versions',
+            '> 5%',
+        ],
+    }),
+];
+
+gulp.task('webserver', () => {
     gulp.src('./dist')
-    .pipe($.serverLivereload({
-        livereload: true,
-        directoryListing: false,
-        port: 3000,
-        open: true
-    }));
+        .pipe($.serverLivereload({
+            livereload: true,
+            directoryListing: false,
+            port: 3000,
+            open: true,
+        }));
 });
 
-gulp.task('copy-html', function () {
-    return gulp.src('./src/index.html')
-    .pipe(gulp.dest('./dist/'))
-});
+gulp.task('copy-html', () => gulp.src('./src/index.html')
+    .pipe(gulp.dest('./dist/')));
 
-gulp.task('copy-js', function () {
-    return gulp.src('./src/js/**/*.js')
-    .pipe(gulp.dest('./dist/js'))
-});
+gulp.task('copy-js', () => gulp.src('./src/js/**/*.js')
+    .pipe(gulp.dest('./dist/js')));
 
 gulp.task('copy', ['copy-html', 'copy-js']);
 
-gulp.task('lint-styles', function () {
-    return gulp.src('./src/scss/**/*.scss')
+gulp.task('lint-styles', () => gulp.src('./src/scss/**/*.scss')
     .pipe($.sassLint())
     .pipe($.sassLint.format())
-    .pipe($.sassLint.failOnError())
-})
+    .pipe($.sassLint.failOnError()));
 
-gulp.task('styles', ['lint-styles'], function () {
-    return gulp.src('./src/scss/**/*.scss')
+gulp.task('styles', ['lint-styles'], () => gulp.src('./src/scss/**/*.scss')
     .pipe($.sass().on('error', $.sass.logError))
     .pipe($.postcss(plugins))
-    .pipe(gulp.dest('./dist/css'))
-});
+    .pipe(gulp.dest('./dist/css')));
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
     gulp.watch('./src/scss/**/*.scss', ['styles']);
     gulp.watch('./src/js/**/*.js', ['copy-js']);
     gulp.watch('./src/index.html', ['copy-html']);
