5c305d8cbe86d6114f55a9ad439adab5a5fba6f5
Murray Nuttall <murray@catchdesign.co.nz>
2018-08-22 15:59:57 +1200

    Updated to browsersync and stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
 const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync').create();
 
 const $ = gulpLoadPlugins();
 
@@ -12,7 +13,7 @@ const eslintFileList = [
     '!src/js/vendor/**',
 ];
 
-const postcssPlugins = [
+const autoprefixerConfig = [
     autoprefixer({
         browsers: [
             'last 2 versions',
@@ -22,42 +23,54 @@ const postcssPlugins = [
 ];
 
 gulp.task('webserver', () => {
-    gulp.src('./dist')
-        .pipe($.serverLivereload({
-            livereload: true,
-            directoryListing: false,
-            port: 3000,
-            open: true,
-        }));
+    browserSync.init({
+        server: {
+            baseDir: './dist/',
+        },
+    });
 });
 
 gulp.task('copy-html', () => gulp.src('./src/index.html')
     .pipe(gulp.dest('./dist/')));
 
+gulp.task('copy-images', () => gulp.src('./src/img/**/*')
+    .pipe(gulp.dest('./dist/img')));
+
+gulp.task('copy-fonts', () => gulp.src('./src/fonts/**/*')
+    .pipe(gulp.dest('./dist/fonts')));
+
 gulp.task('copy-js', ['lint-js'], () => gulp.src('./src/js/**/*.js')
     .pipe(gulp.dest('./dist/js')));
 
-gulp.task('copy', ['copy-html', 'copy-js']);
+gulp.task('copy', ['copy-images', 'copy-fonts', 'copy-html', 'copy-js']);
 
 gulp.task('lint-styles', () => gulp.src('./src/scss/**/*.scss')
-    .pipe($.sassLint())
-    .pipe($.sassLint.format())
-    .pipe($.sassLint.failOnError()));
+    .pipe($.stylelint({
+        reporters: [{
+            formatter: 'string',
+            console: true,
+        }],
+    })));
 
 gulp.task('lint-js', () => gulp.src(eslintFileList)
     .pipe($.eslint({ useEslintrc: true }))
     .pipe($.eslint.format()));
 
-gulp.task('styles', ['lint-styles'], () => gulp.src('./src/scss/**/*.scss')
-    .pipe($.sass().on('error', $.sass.logError))
-    .pipe($.postcss(postcssPlugins))
-    .pipe(gulp.dest('./dist/css')));
+gulp.task('styles', () => gulp.src('./src/scss/**/*.scss')
+    .pipe($.sass({
+        precision: 8,
+        includePaths: ['node_modules/normalize-scss/sass/normalize'],
+    }).on('error', $.sass.logError))
+    .pipe($.postcss(autoprefixerConfig))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.reload({ stream: true })));
 
 gulp.task('watch', () => {
     gulp.watch('./src/scss/**/*.scss', ['styles']);
     gulp.watch('./src/js/**/*.js', ['copy-js']);
-    gulp.watch('./src/index.html', ['copy-html']);
+    gulp.watch('./src/index.html', ['copy-html']).on('change', browserSync.reload);
 });
 
 gulp.task('build', ['copy', 'styles']);
+gulp.task('test', ['lint-styles', 'lint-js']);
 gulp.task('default', ['copy', 'styles', 'watch', 'webserver']);
