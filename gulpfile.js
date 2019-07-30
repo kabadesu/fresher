c52ff6988693cd38cd7efdb759cc977b5505a7fe
Murray Nuttall <murray.nuttall@gmail.com>
2019-07-31 11:04:53 +1200

    Upgraded to twing and gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,41 @@ const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
 const fs = require('fs');
 
-const $ = gulpLoadPlugins();
+const { TwingEnvironment, TwingLoaderFilesystem } = require('twing');
+
+const $ = gulpLoadPlugins({
+    pattern: [
+        '*',
+        '!stylelint',
+        '!twing',
+        '!eslint'
+    ],
+    lazy: true,
+});
 
 const _ = {
     src: './src',
     dist: './dist',
 };
+
+let siteData = require(`${_.src}/data.json`);
+
+const twingInit = () => {
+    const loader = new TwingLoaderFilesystem('.');
+
+    loader.addPath(`${_.src}/twig/components/`, 'components');
+    loader.addPath(`${_.src}/twig/layouts/`, 'layouts');
+    loader.addPath(`${_.src}/img/`, 'image');
+
+    const env = new TwingEnvironment(loader);
+    env.addGlobal('imagepath', '/img');
+
+    return {
+        loader, env
+    };
+}
+
+let { loader, env } = twingInit();
 
 // gulp-eslint can read from .eslintignore but it makes the task run slower.
 const eslintFileList = [
@@ -19,29 +48,27 @@ const eslintFileList = [
     `!${_.src}/js/vendor/**`,
 ];
 
-const autoprefixerConfig = [
-    autoprefixer({
-        browsers: [
-            'last 2 versions',
-            '> 1%',
-        ],
-    }),
+const postcssPlugins = [
+    autoprefixer(),
 ];
 
-gulp.task('webserver', () => {
-    browserSync.init({
+function webserver() {
+    return browserSync.init({
         server: {
             baseDir: _.dist,
         },
         port: 8000,
         // ghostMode: false,
-    });
-});
+    })
+}
 
-gulp.task('clean', () => gulp.src(`${_.dist}*`, { read: false })
-    .pipe($.clean()));
+function clean() {
+    return gulp.src(`${_.dist}*`, { read: false })
+    .pipe($.clean())
+}
 
-gulp.task('icons', () => gulp.src(`${_.src}/icons/**/*.svg`)
+function icons() {
+    return gulp.src(`${_.src}/icons/**/*.svg`)
     .pipe($.cheerio({
         // I'd usually use $ here, but es6lint is complaining about it so...
         run: (ಠ_ಠ) => {
@@ -62,59 +89,69 @@ gulp.task('icons', () => gulp.src(`${_.src}/icons/**/*.svg`)
     }))
     .pipe($.svgstore())
     .pipe($.rename('icons.svg'))
-    .pipe(gulp.dest(`${_.dist}/img`)));
+    .pipe(gulp.dest(`${_.dist}/img`))
+}
 
-gulp.task('twig', () => gulp.src(`${_.src}/twig/pages/**/*.twig`)
-    .pipe($.data(() => JSON.parse(fs.readFileSync(`${_.src}/data.json`))))
-    .pipe($.twig({
-        base: `${_.src}/twig/`,
-    }))
-    .on('error', function handleError(error) {
-        console.log(error.toString());
-        this.emit('end');
-    })
-    .pipe(gulp.dest(_.dist)));
+function twig() {
+    return gulp.src(`${_.src}/twig/pages/**/*.twig`)
+        .pipe($.twing(env, siteData))
+        .pipe(gulp.dest(_.dist))
+}
 
-gulp.task('styles', () => gulp.src(`${_.src}/scss/main.scss`)
+function styles() {
+    return gulp.src(`${_.src}/scss/main.scss`)
     .pipe($.sass({
         precision: 8,
         includePaths: ['node_modules/normalize-scss/sass/normalize'],
     }).on('error', $.sass.logError))
-    .pipe($.postcss(autoprefixerConfig))
+    .pipe($.postcss(postcssPlugins))
     .pipe(gulp.dest(`${_.dist}/css`))
-    .pipe(browserSync.reload({ stream: true })));
+    .pipe(browserSync.reload({ stream: true }))
+}
 
-gulp.task('stylelint', () => gulp.src(`${_.src}/scss/**/*.scss`)
+function stylelint() {
+    return gulp.src(`${_.src}/scss/**/*.scss`)
     .pipe($.stylelint({
         reporters: [{
             formatter: 'string',
             console: true,
         }],
-    })));
+    }))
+}
 
-gulp.task('lint-js', () => gulp.src(eslintFileList)
+function lintJs() {
+    return gulp.src(eslintFileList)
     .pipe($.eslint({ useEslintrc: true }))
-    .pipe($.eslint.format()));
+    .pipe($.eslint.format())
+}
 
-gulp.task('copy-images', () => gulp.src(`${_.src}/img/**/*`)
-    .pipe(gulp.dest(`${_.dist}/img`)));
+function copyImages() {
+    return gulp.src(`${_.src}/img/**/*`)
+    .pipe(gulp.dest(`${_.dist}/img`))
+}
 
-gulp.task('copy-fonts', () => gulp.src(`${_.src}/fonts/**/*`)
-    .pipe(gulp.dest(`${_.dist}/fonts`)));
+function copyFonts() {
+    return gulp.src(`${_.src}/fonts/**/*`)
+    .pipe(gulp.dest(`${_.dist}/fonts`))
+}
 
-gulp.task('copy-js', ['lint-js'], () => gulp.src(`${_.src}/js/**/*.js`)
-    .pipe(gulp.dest(`${_.dist}/js`)));
+function copyJs() {
+    return gulp.src(`${_.src}/js/**/*.js`)
+    .pipe(gulp.dest(`${_.dist}/js`))
+}
 
-gulp.task('copy', ['copy-images', 'copy-fonts', 'copy-js']);
+gulp.task('copy', gulp.series(copyImages, copyFonts, lintJs, copyJs));
 
 gulp.task('watch', () => {
-    gulp.watch(`${_.src}/scss/**/*.scss`, ['styles']);
-    gulp.watch(`${_.src}/js/**/*.js`, ['copy-js']).on('change', browserSync.reload);
-    gulp.watch(`${_.src}/icons/**/*.svg`, ['icons']);
-    gulp.watch(`${_.src}/data.json`, ['twig']).on('change', browserSync.reload);
-    gulp.watch(`${_.src}/twig/**/*.njk`, ['twig']).on('change', browserSync.reload);
+    gulp.watch(`${_.src}/scss/**/*.scss`, gulp.series(styles));
+    gulp.watch(`${_.src}/js/**/*.js`, gulp.series('copy-js')).on('change', browserSync.reload);
+    gulp.watch(`${_.src}/icons/**/*.svg`, gulp.series(icons));
+    gulp.watch(`${_.src}/data.json`, gulp.series(twig)).on('change', browserSync.reload);
+    gulp.watch(`${_.src}/twig/**/*.twig`, gulp.series(twig)).on('change', browserSync.reload);
 });
 
-gulp.task('build', ['copy', 'twig', 'styles']);
-gulp.task('test', ['stylelint', 'lint-js']);
-gulp.task('default', ['copy', 'twig', 'icons', 'styles', 'watch', 'webserver']);
+gulp.task('clean', clean);
+
+gulp.task('build', gulp.series('copy', twig, styles));
+gulp.task('test', gulp.series(stylelint, lintJs));
+gulp.task('default', gulp.series('copy', twig, icons, styles, gulp.parallel('watch', webserver)));
